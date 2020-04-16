5e1b3c074dc8aea4feb522b795dd1daaebf0e0ae
matt <mhofmann@hawk.iit.edu>
2020-04-15 20:50:27 -0500

    Complete dashboard design

--- dashboard.py
+++ dashboard.py
@@ -1,23 +1,106 @@
-#osint project dashboard
-#Developed by Matt Hofmann, Simeon Wilson, and Zachary Fleck
+# osint project dashboard
+# Developed by Matt Hofmann, Simeon Wilson, and Zachary Fleck
 import tkinter as tk
 
-#initialize frame
+# initialize frame
 win = tk.Tk()
 win.title("OSINT Dashboard")
-win.geometry('300x300')
-#make window not resizable
-win.resizable(0,0)
-#label
+win.geometry('300x350')
+# make window not resizable
+win.resizable(0, 0)
+v = tk.IntVar()
+
+
+def showChoice():
+    print(v.get())
+
+
+#reddit fields assigned to variables outside of reddittext() method
+r1 = tk.Label(win, text="Search")
+r2 = tk.Label(win, text="Subreddit")
+r3 = tk.Label(win, text="")
+r4 = tk.Label(win, text="Sort results by")
+r5 = tk.Radiobutton(win, text="Hot", variable=v, value=1, command=showChoice())
+r6 = tk.Radiobutton(win, text="New", variable=v, value=2, command=showChoice())
+r7 = tk.Radiobutton(win, text="Top", variable=v, value=3, command=showChoice())
+r8 = tk.Radiobutton(win, text="Controversial", variable=v, value=4, command=showChoice())
+r9 = tk.Radiobutton(win, text="Rising", variable=v, value=5, command=showChoice())
+e1 = tk.Entry(win)
+e2 = tk.Entry(win)
+submitreddit = tk.Button(win, text="Submit")
+
+
+# reddit text entry field
+def reddittext():
+    #removes twitter residue
+    t1.grid_forget()
+    t2.grid_forget()
+    t3.grid_forget()
+    t4.grid_forget()
+    t5.grid_forget()
+    e1.grid_forget()
+    submittwit.grid_forget()
+    # places reddit fields on the window
+    r1.grid(row=3, column=1)
+    r2.grid(row=4, column=1)
+    r3.grid(row=5, column=1)
+    r4.grid(row=6, column=1)
+    r5.grid(row=7, column=1)
+    r6.grid(row=8, column=1)
+    r7.grid(row=9, column=1)
+    r8.grid(row=10, column=1)
+    r9.grid(row=11, column=1)
+    e1.grid(row=3, column=2)
+    e2.grid(row=4, column=2)
+    submitreddit.grid(row=12, column=2)
+
+
+#twitter fields assigned to variables outside of twittertext() method
+t1 = tk.Label(win, text="Search")
+e1 = tk.Entry(win)
+t2 = tk.Label(win, text="")
+t5 = tk.Label(win, text="Sort results by")
+t3 = tk.Radiobutton(win, text="New", variable=v, value=1)
+t4 = tk.Radiobutton(win, text="Top", variable=v, value=2)
+submittwit = tk.Button(win, text="Submit")
+
+
+# twitter text entry field
+def twittertext():
+    # removes reddit residue
+    r1.grid_forget()
+    r2.grid_forget()
+    r3.grid_forget()
+    r4.grid_forget()
+    r5.grid_forget()
+    r6.grid_forget()
+    r7.grid_forget()
+    r9.grid_forget()
+    r8.grid_forget()
+    e1.grid_forget()
+    e2.grid_forget()
+    submitreddit.grid_forget()
+    #places twitter fields on the window
+    t1.grid(row=3, column=1)
+    e1.grid(row=3, column=2)
+    e1.grid(row=3, column=2)
+    t2.grid(row=4, column=1)
+    t5.grid(row=5, column=1)
+    t3.grid(row=6, column=1)
+    t4.grid(row=7, column=1)
+    submittwit.grid(row=7, column=2)
+
+
+# label
 label1 = tk.Label(win, text="Please select a site").grid(column=1, row=1)
-#reddit button
-action = tk.Button(win, text="Reddit", bg='#FF4301', activebackground='#FF4301')
+# reddit button
+action = tk.Button(win, text="Reddit", bg='#FF4301', activebackground='#FF4301', command=reddittext)
 action.grid(column=1, row=2)
 action.config(height='4', width='20')
-#twitter button
-action2 = tk.Button(win, text="Twitter", bg='#00acee', activebackground='#00acee')
+# twitter button
+action2 = tk.Button(win, text="Twitter", bg='#00acee', activebackground='#00acee', command=twittertext)
 action2.grid(column=2, row=2)
 action2.config(height='4', width='20')
 
-#show gui
+# show gui
 win.mainloop()
